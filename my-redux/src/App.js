435f1c8e6cb07b5c477a44f78b7b755507bb9fac
Vivek3927 <vivek.vas27@gmail.com>
2020-11-07 13:01:36 +0530

    React Redux

---
+++ my-redux/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Counter from "./Containers/Counter/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-redux/src/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import counterReducer from "./Store/reducers/counter";
+import resultReducer from "./Store/reducers/result";
+import { Provider } from "react-redux";
+
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer,
+});
+
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("[MiddleWare] Dispatching", action);
+      const result = next(action);
+      console.log("Result ", result);
+      console.log("[MiddleWare] next state", store.getState());
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
